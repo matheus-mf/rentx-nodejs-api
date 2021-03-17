e2785c35780a836b1b90016ba64f569970be0da2
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-16 23:42:05 -0300

    feat(category): model creation

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,20 @@
 import { Router } from "express";
-import { v4 as uuidV4 } from "uuid";
+
+import { Category } from "../model/Category";
 
 const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = {
-    id: uuidV4(),
+  const category = new Category();
+  Object.assign(category, {
     name,
     description,
-  };
+    created_ad: new Date(),
+  });
 
   categories.push(category);
 
