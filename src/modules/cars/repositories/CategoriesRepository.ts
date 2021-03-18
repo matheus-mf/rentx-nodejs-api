9a0a5698ef084577dd74b5011a419e3f492225aa
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-17 21:49:00 -0300

    refactor(cars): implementation of the singleton pattern in category

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -7,8 +7,17 @@ import {
 export default class CategoriesRepository implements ICategoriesRepository {
   private readonly categories: Category[];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+    return CategoriesRepository.INSTANCE;
   }
 
   list(): Category[] {
