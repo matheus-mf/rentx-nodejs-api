c98d4ea9f2305e46498b28a8d444e42fa51b0881
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-24 00:33:31 -0300

    feat(user): migration to create categories

---
+++ src/database/migrations/1616554245466-CreateCategories.ts
@@ -0,0 +1,40 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategories1616554245466 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "categories",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "crated_at",
+            type: "timestamp",
+            default: "now()",
+          },
+          {
+            name: "update_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories");
+  }
+}
