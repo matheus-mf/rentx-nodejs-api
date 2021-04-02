6585c67ba6b7cf2b7487eacf211dcdd6fa8f678e
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-04-01 21:09:25 -0300

    test(cars): unit test for create category

---
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,30 @@
+import Category from "../../entities/Category";
+import {
+  ICategoriesRepository,
+  ICreateCategoryDTO,
+} from "../ICategoriesRepository";
+
+class CategoriesRepositoryInMemory implements ICategoriesRepository {
+  private categories: Category[] = [];
+
+  async findByName(name: string): Promise<Category> {
+    return this.categories.find((category) => category.name === name);
+  }
+
+  async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+    });
+
+    this.categories.push(category);
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories;
+  }
+}
+
+export { CategoriesRepositoryInMemory };
