55f3eded24f3b0a69e7577a6f180f414dc9309e4
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-04-04 16:18:56 -0300

    feat(cars): creating middleware for ensure user is admin

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -4,6 +4,8 @@ import multer from "multer";
 import CreateCategoryController from "@modules/cars/useCases/createCategory/CreateCategoryController";
 import ImportCategoryController from "@modules/cars/useCases/importCategory/ImportCategoryController";
 import ListCategoriesController from "@modules/cars/useCases/listCategories/ListCategoriesController";
+import ensureAdmin from "@shared/infra/http/middlewares/ensureAdmin";
+import ensureAuthenticated from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const categoriesRoutes = Router();
 
@@ -17,11 +19,18 @@ const listCategoriesController = new ListCategoriesController();
 
 categoriesRoutes.get("/", listCategoriesController.handle);
 
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle
+);
 
 categoriesRoutes.post(
   "/import",
   upload.single("file"),
+  ensureAuthenticated,
+  ensureAdmin,
   importCategoryController.handle
 );
 
