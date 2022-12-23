c6c46b50fe050bb5c8249c07555ea30730bf4015
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-12-23 15:01:58 -0300

    feat(refresh-token): repositório

---
+++ src/modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory.ts
@@ -0,0 +1,28 @@
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import UserToken from "@modules/accounts/infra/typeorm/entities/UserToken";
+
+import { IUsersTokensRepository } from "../IUsersTokensRepository";
+
+class UsersTokensRepositoryInMemory implements IUsersTokensRepository {
+  private usersTokens: UserToken[] = [];
+
+  async create({
+    expires_date,
+    refresh_token,
+    user_id,
+  }: ICreateUserTokenDTO): Promise<UserToken> {
+    const userToken = new UserToken();
+
+    Object.assign(userToken, {
+      expires_date,
+      refresh_token,
+      user_id,
+    });
+
+    this.usersTokens.push(userToken);
+
+    return userToken;
+  }
+}
+
+export { UsersTokensRepositoryInMemory };
