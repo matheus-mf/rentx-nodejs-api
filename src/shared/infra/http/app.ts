17fa22e23381a91e4062198dc631becd04f39a4e
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-12-26 18:12:38 -0300

    feat: add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,15 +1,16 @@
 import "reflect-metadata";
 import "dotenv/config";
 
+import cors from "cors";
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
+import upload from "@config/upload";
 import AppError from "@shared/errors/AppError";
 import createConnection from "@shared/infra/typeorm";
-import "@shared/container";
 
-import upload from "@config/upload";
+import "@shared/container";
 
 import swaggerFile from "../../../swagger.json";
 import router from "./routes";
@@ -24,6 +25,7 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
