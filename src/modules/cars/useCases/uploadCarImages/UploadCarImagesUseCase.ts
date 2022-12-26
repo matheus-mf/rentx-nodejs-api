7c4455b0d401562345caa19d2e4c8e8453133230
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2022-12-26 10:06:54 -0300

    fix(cars): upload imgs, using storage provider

--- src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStorageProvider";
 
 interface IRequest {
   car_id: string;
@@ -11,12 +12,15 @@ interface IRequest {
 export default class UploadCarImagesUseCase {
   constructor(
     @inject("CarsImagesRepository")
-    private carsImageRepository: ICarsImagesRepository
+    private carsImageRepository: ICarsImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
     images_name.map(async (image) => {
       await this.carsImageRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
